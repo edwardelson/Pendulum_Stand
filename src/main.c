4ae64020ba72cf32bd77198158fba81b8832593b
edwardelson <edward_elson@rocketmail.com>
2015-09-24 14:15:05 +0800

    deleted printf

--- src/main.c
+++ src/main.c
@@ -54,8 +54,6 @@ void task4Function(void const * argument);
 /* Main ----------------------------------------------------------------------*/
 int main(void)
 {
-	printf("test");
-
 	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
 	HAL_Init();
 
